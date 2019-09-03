3431b787113725322ecabece880700eb7ae22555
unknown <mmedina8062@gmail.com>
2019-09-03 15:53:25 -0500

    added rules to LemonadeGame class

--- LemonadeStand/LemonadeGame.cs
+++ LemonadeStand/LemonadeGame.cs
@@ -7,5 +7,16 @@ namespace LemonadeStand
 {
     public class LemonadeGame
     {
+        string Player;
+        string Store;
+        string GameRules;
+    }
+
+    public void GameRules()
+    {
+        Console.WriteLine("You have 7 days to make as much money as possible from your Lemonade Stand.\n");
+        Console.WriteLine("These days will have weather conditions, so keep in mind you inventory and sale price should be based off of the weather for that days.\n");
+        Console.WriteLine("You do have control over your inventory, ingredients, and sale price of your lemonade.\n");
+        
     }
 }
